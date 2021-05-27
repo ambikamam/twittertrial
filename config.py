d6fe6c6019ae08c39a0812753f4fb265ed9d9956
LearnerAmb <ambika.mam12@gmail.com>
2021-05-27 11:17:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-consumer_key = '1rutjqltwbJhGn1GquehHLbi7'
-consumer_secret = '8O8FwPkomNWn4CdkbSYpmWJddklZ4n2gjvnb2gjfw1fCeB02wL'
-access_token = '514470357-x0sMQTcMViGWEBnKz1ZaydqzvdA4sqW1Dl3XyT85'
-access_token_secret = 'SSNZXUa831FsTZYf4iI8X19tFIz8zVsd6sS6jxCd2VKuX'
+consumer_key = 'XXXXX'
+consumer_secret = 'XXXX'
+access_token = 'XXXX'
+access_token_secret = 'XXXX'
 #auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 #auth.set_access_token(access_token, access_token_secret)
 #api = tweepy.API(auth,wait_on_rate_limit=True)
